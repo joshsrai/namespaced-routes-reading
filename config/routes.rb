7f778c587604c0947bf961d421c61931777f8706
joshsrai <joshsrai@gmail.com>
2021-01-06 08:45:50 -0500

    commit tree 2f3bd546be3a52875baf731e6376724b2629db73

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,38 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  # get '/stats', to: 'stats#index' 
+  # added stats#index
+  # but then was moved to admin/stats
+  # so that only the admin can see the stats
+
+  # get '/admin/stats', to: 'stats#index'
+  # the above route was refactored in favor of using scope,
+  # so that DRY could be implemented
+  
+  # scope '/admin' do
+  #   resources :stats, only: [:index]
+  # end 
+  # because the index stats view file was moved into
+  # app/views/admin/stats/index.html.erb
+  # and the stats_contoller was moved into
+  # app/controllers/admin/stats_controller.rb
+  # scope needed to be update to show the change
+
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end 
+    # When we want to route with a module
+    # and use that module's name as the URL prefix,
+    # we can use the namespace method instead of
+    # scope, module.
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end 
 
   root 'posts#index'
+
+  
+
 end
